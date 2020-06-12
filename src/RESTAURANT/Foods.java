094f085c1c18b7cafbe48e3a974fcfa446836c98
NYessengeldi <54642111+NYessengeldi@users.noreply.github.com>
2020-06-12 15:34:06 +0600

    Foods implementation

--- src/RESTAURANT/Foods.java
+++ src/RESTAURANT/Foods.java
@@ -3,13 +3,40 @@ package RESTAURANT;
 import java.util.ArrayList;
 
 public class Foods {
+    private int id;
+    private static int id_gen = 0;
     private String name;
     private int price;
     private Receipts receipt;
     public Foods(String name, int price, Receipts receipt){
+        id =  id_gen++;
         this.name = name;
         this.price = price;
         this.receipt = receipt;
     }
+    public int getId(){
+        return id;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
+    public int getPrice(){
+        return price;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setReceipt(Receipts receipt) {
+        this.receipt = receipt;
+    }
+
+    public Receipts getReceipt() {
+        return receipt;
+    }
 }
